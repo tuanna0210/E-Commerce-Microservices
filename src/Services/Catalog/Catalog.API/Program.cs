2688c3560722aa57e7d6ca5f01100c6c7e2b2a16
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-12 15:24:04 +0700

    BuildingBlock: Add ValidationBehavior Catalog.API: Config ValidationBehavior using mediatR pipelineBehavior

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,13 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 //Add services to the container
 builder.Services.AddCarter();
 builder.Services.AddMediatR(cfg =>
 {
     cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(opts =>
