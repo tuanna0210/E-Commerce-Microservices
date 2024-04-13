74d3867c664d13ca133e93299a7b1787295348f6
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-13 14:53:19 +0700

    Catalog.API: Add healthchecks

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,6 @@
 using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add services to the container
@@ -26,10 +28,18 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+//-Healthcheck
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
 app.MapCarter();
 app.UseExceptionHandler(opts => { });
-
+//-Healthcheck
+app.UseHealthChecks("/health", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 app.Run();
