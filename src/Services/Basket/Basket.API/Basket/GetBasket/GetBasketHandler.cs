f43e97e1e0dd76c7f9e7acca5d6934c2b6c6f200
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-17 21:48:24 +0700

    basket.api: GetBasket feature p2

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,13 +1,16 @@
 ﻿
+using Basket.API.Data;
+
 namespace Basket.API.Basket.GetBasket
 {
     public record GetBasketQuery(string UserName): IQuery<GetBasketResult>;
     public record GetBasketResult(ShoppingCart Cart);
-    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    public class GetBasketQueryHandler(IBasketRepository repository) : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
         public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
         {
-            return new GetBasketResult(new ShoppingCart("abc"));
+            var basket = await repository.GetBasket(query.UserName, cancellationToken);
+            return new GetBasketResult(basket);
         }
     }
 }
