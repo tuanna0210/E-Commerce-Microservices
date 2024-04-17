7ce618fdaeb3d3c2c8ac88d60dda5b17e8886881
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-17 14:34:32 +0700

    Basket.API: StoreBasket feature p1

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,27 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+
+
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart is required");
+            RuleFor(x => x.Cart.Username).NotEmpty().WithMessage("Username is required");
+        }
+    }
+    public class StoreBasketCommandHandler(IDocumentSession session) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+            //TODO: Store shopping cart
+            //TODO: Update cache
+            return new StoreBasketResult("abc");
+        }
     }
 }
--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -2,3 +2,5 @@
 global using MediatR;
 global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using Marten;
