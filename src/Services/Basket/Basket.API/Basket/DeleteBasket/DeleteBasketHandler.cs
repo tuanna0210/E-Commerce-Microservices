242915b75ce48e848dddaf68d39d048109f68f5b
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-17 15:05:15 +0700

    Basket.API: DeleteBasket feature p1

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,22 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName): ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator: AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+        {
+            //TODO: Delete basket from DB and cache
+            return new DeleteBasketResult(true);
+        }
     }
 }
