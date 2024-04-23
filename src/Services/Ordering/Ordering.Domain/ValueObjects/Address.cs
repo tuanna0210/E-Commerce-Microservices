1963a57db269533d643fe8e76d5d4d0c3da84c15
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-23 10:42:30 +0700

    Ordering.Domain: Apply Rich-Domain model for all entities, valueObjects

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -3,11 +3,35 @@
     public record Address
     {
         public string FirstName { get; } = default!;
-        public string LastNamae { get; } = default!;
+        public string LastName { get; } = default!;
         public string? EmailAddress { get; } = default!;
         public string AddressLine { get; } = default!;
         public string Country { get; } = default!;
         public string State { get; } = default!;
         public string Zipcode { get; } = default!;
+
+        protected Address()
+        {
+
+        }
+
+        private Address(string firstName, string lastName, string? emailAddress, string addressLine, string country, string state, string zipcode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            AddressLine = addressLine;
+            Country = country;
+            State = state;
+            Zipcode = zipcode;
+        }
+        public static Address Of(string firstName, string lastName, string? emailAddress, string addressLine, string country, string state, string zipcode)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(emailAddress);
+            ArgumentException.ThrowIfNullOrEmpty(addressLine);
+
+
+            return new Address(firstName, lastName, emailAddress, addressLine, country, state, zipcode);
+        }
     }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordering.Domain.ValueObjects
+﻿using System.Net.Mail;
+
+namespace Ordering.Domain.ValueObjects
 {
     public record Payment
     {
@@ -7,5 +9,27 @@
         public string Expiration { get; } = default!;
         public string CVV { get; } = default!;
         public int PaymentMethod { get; } = default!;
+        protected Payment()
+        {
+
+        }
+
+        private Payment(string? cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            CVV = cvv;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string? cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(cardName);
+            ArgumentException.ThrowIfNullOrEmpty(cardNumber);
+            ArgumentException.ThrowIfNullOrEmpty(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+        }
     }
 }
