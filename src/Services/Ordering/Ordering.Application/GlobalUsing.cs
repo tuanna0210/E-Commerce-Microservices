67c0555623fd8e88af298dcc11fde3ff338ec500
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-25 10:49:01 +0700

    Ordering.Application: CreateOrderHander p2

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,2 +1,6 @@
 ﻿global using BuildingBlocks.CQRS;
 global using FluentValidation;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
