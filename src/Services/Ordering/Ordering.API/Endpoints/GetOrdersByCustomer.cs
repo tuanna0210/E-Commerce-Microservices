894c0b3adb07b31e4cdc146be76a14a443572e2a
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-25 21:13:13 +0700

    Ordering.API: GetOrdersByCustomer Endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -1,6 +1,28 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+using Ordering.Application.Orders.Queries.GetOrdersByName;
+
+namespace Ordering.API.Endpoints
 {
-    public class GetOrdersByCustomer
+    //public record GetOrdersByCustomerRequest(Guid CustomerId);
+    public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByCustomer : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+
+                var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetOrdersByCustomer")
+            .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Orders By Customer")
+            .WithDescription("Get Orders By Customer");
+        }
     }
 }
