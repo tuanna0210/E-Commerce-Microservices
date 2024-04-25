19b39abb0f0780e2e2a43c38392f0e55afa6b4de
Tuan Nguyen <anhtuann0210@gmail.com>
2024-04-25 14:12:47 +0700

    Ordering.Application: UpdateOrderCommand

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,3 +4,5 @@ global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Application.Exceptions;
+
